a9ebf536914f4fa2a5405641011ea65cc8e18393
yulemao <18210526906@163.com>
2024-04-15 19:14:51 +0800

    Update fast_gelu_tiling.h

--- FastGelu/op_host/fast_gelu_tiling.h
+++ FastGelu/op_host/fast_gelu_tiling.h
@@ -5,6 +5,12 @@ namespace optiling {
 BEGIN_TILING_DATA_DEF(FastGeluTilingData)
   TILING_DATA_FIELD_DEF(uint32_t, totalLength);
   TILING_DATA_FIELD_DEF(uint32_t, tileNum);
+  TILING_DATA_FIELD_DEF(uint32_t, ALIGN_NUM);
+  TILING_DATA_FIELD_DEF(uint32_t, tiling_size);
+  TILING_DATA_FIELD_DEF(uint32_t, block_size);
+  TILING_DATA_FIELD_DEF(uint32_t, aivNum);
+  TILING_DATA_FIELD_DEF(uint32_t, core_size);
+  TILING_DATA_FIELD_DEF(uint32_t, core_remain);
 END_TILING_DATA_DEF;
 
 REGISTER_TILING_DATA_CLASS(FastGelu, FastGeluTilingData)

487604b23aa0017eaeda919941f31c3a13882f3f
smz <531483935@qq.com>
2024-04-17 22:40:30 +0800

    Implement LpNormV2

---
+++ LpNormV2/op_host/lp_norm_v2.cpp
@@ -0,0 +1,115 @@
+
+#include "lp_norm_v2_tiling.h"
+#include "register/op_def_registry.h"
+#include "tiling/platform/platform_ascendc.h"
+#include <cmath>
+
+
+namespace optiling {
+const uint32_t BLOCK_SIZE = 32;
+static ge::graphStatus TilingFunc(gert::TilingContext* context)
+{
+    LpNormV2TilingData tiling;
+    constexpr int32_t NUM = 8;
+    uint32_t sizeofdatatype;
+    uint32_t totalLengthAligned;
+    auto ascendcPlatform = platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
+    auto socVersion = ascendcPlatform.GetSocVersion();
+    uint64_t ub_size;
+    ascendcPlatform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, ub_size);
+    auto aivNum = ascendcPlatform.GetCoreNum();
+
+    uint32_t totalLength = context->GetInputTensor(0)->GetShapeSize();
+    auto dt = context->GetInputTensor(0)->GetDataType();
+    if(dt == ge::DT_INT8){
+        sizeofdatatype = 1;
+    }else if(dt == ge::DT_FLOAT16 || dt == ge::DT_BF16){
+        sizeofdatatype = 2;
+    }else{
+        sizeofdatatype = 4;
+    }
+
+    auto p = context->GetAttrs()->GetFloat(0);
+    tiling.set_p(*p);
+    if(*p == 0){
+        tiling.set_ptype(1);
+    }else if(std::isinf(*p) && *p > 0){
+        tiling.set_ptype(2);
+    }else if(std::isinf(*p) && *p < 0){
+        tiling.set_ptype(3);
+    }else{
+        tiling.set_ptype(0);
+    }
+
+    uint32_t ALIGN_NUM = BLOCK_SIZE / sizeofdatatype;
+    uint32_t tiling_size = ((ub_size) / BLOCK_SIZE / 2) / NUM;
+
+    uint32_t block_size = tiling_size * ALIGN_NUM;
+    aivNum = (aivNum < totalLength / block_size) ? aivNum : (totalLength / block_size);
+    aivNum = aivNum >= 1 ? aivNum : 1;
+    aivNum = 1;
+
+    uint32_t core_size = totalLength / aivNum;
+    uint32_t core_remain = totalLength % aivNum;
+
+    tiling.set_totalLength(totalLength);
+    tiling.set_ALIGN_NUM(ALIGN_NUM);
+    tiling.set_tiling_size(tiling_size);
+    tiling.set_block_size(block_size);
+    tiling.set_aivNum(aivNum);
+    tiling.set_core_size(core_size);
+    tiling.set_core_remain(core_remain);
+
+    context->SetBlockDim(aivNum);
+
+    tiling.SaveToBuffer(context->GetRawTilingData()->GetData(), context->GetRawTilingData()->GetCapacity());
+    context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
+    size_t *currentWorkspace = context->GetWorkspaceSizes(1);
+    currentWorkspace[0] = 0;
+    return ge::GRAPH_SUCCESS;
+}
+}
+
+
+namespace ge {
+static ge::graphStatus InferShape(gert::InferShapeContext* context)
+{
+    const gert::Shape* x1_shape = context->GetInputShape(0);
+    gert::Shape* y_shape = context->GetOutputShape(0);
+    *y_shape = *x1_shape;
+    return GRAPH_SUCCESS;
+}
+}
+
+
+namespace ops {
+class LpNormV2 : public OpDef {
+public:
+    explicit LpNormV2(const char* name) : OpDef(name)
+    {
+        this->Input("x")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Output("y")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Attr("p").AttrType(OPTIONAL).Float(2);
+        this->Attr("axes").AttrType(OPTIONAL).ListInt({});
+        this->Attr("keepdim").AttrType(OPTIONAL).Bool(false);
+        this->Attr("epsilon").AttrType(OPTIONAL).Float(1e-12);
+
+        this->SetInferShape(ge::InferShape);
+
+        this->AICore()
+            .SetTiling(optiling::TilingFunc);
+        this->AICore().AddConfig("ascend310b");
+
+    }
+};
+
+OP_ADD(LpNormV2);
+}

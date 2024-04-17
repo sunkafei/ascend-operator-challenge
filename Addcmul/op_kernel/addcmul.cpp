b5c8e0f75f10ab7daa0781938c0955fd1b13db24
smz <531483935@qq.com>
2024-04-17 19:55:33 +0800

    Change this->length to a param.

--- Addcmul/op_kernel/addcmul.cpp
+++ Addcmul/op_kernel/addcmul.cpp
@@ -4,7 +4,8 @@ using namespace AscendC;
 constexpr int32_t BUFFER_NUM = 2;                                     // tensor num for each queue
 template<typename T> struct Map {using type = T;};
 template<> struct Map<int8_t> {using type = half;};
-template<typename T> class KernelAddcmul {
+template<typename TYPE_INPUT_DATA, typename TYPE_X1, typename TYPE_X2, typename TYPE_VALUE, typename TYPE_Y> class KernelAddcmul {
+    using T = TYPE_Y;
 public:
     __aicore__ inline KernelAddcmul() {}
     __aicore__ inline void Init(GM_ADDR input_data, GM_ADDR x1, GM_ADDR x2, GM_ADDR value, GM_ADDR y, uint32_t totalLength, uint32_t ALIGN_NUM, uint32_t block_size, uint32_t core_size, uint32_t core_remain) {
@@ -16,18 +17,18 @@ public:
         auto startPointer = this->blockLength * GetBlockIdx() + (GetBlockIdx() < core_remain ? GetBlockIdx() : core_remain);
         auto bufferlength = this->blockLength;
 
-        Gm_input_data.SetGlobalBuffer((__gm__ DTYPE_INPUT_DATA*)input_data + startPointer, bufferlength);
-        Gm_x1.SetGlobalBuffer((__gm__ DTYPE_X1*)x1 + startPointer, bufferlength);
-        Gm_x2.SetGlobalBuffer((__gm__ DTYPE_X2*)x2 + startPointer, bufferlength);
-        Gm_value.SetGlobalBuffer((__gm__ DTYPE_VALUE*)value, 1);
-        Gm_y.SetGlobalBuffer((__gm__ DTYPE_Y*)y + startPointer, bufferlength);
+        Gm_input_data.SetGlobalBuffer((__gm__ TYPE_INPUT_DATA*)input_data + startPointer, bufferlength);
+        Gm_x1.SetGlobalBuffer((__gm__ TYPE_X1*)x1 + startPointer, bufferlength);
+        Gm_x2.SetGlobalBuffer((__gm__ TYPE_X2*)x2 + startPointer, bufferlength);
+        Gm_value.SetGlobalBuffer((__gm__ TYPE_VALUE*)value, 1);
+        Gm_y.SetGlobalBuffer((__gm__ TYPE_Y*)y + startPointer, bufferlength);
 
         this->tileNum = this->blockLength / this->tileLength + (this->blockLength % this->tileLength > 0);
 
-        pipe.InitBuffer(Q_input_data, BUFFER_NUM, this->tileLength * sizeof(DTYPE_INPUT_DATA));
-        pipe.InitBuffer(Q_x1, BUFFER_NUM, this->tileLength * sizeof(DTYPE_X1));
-        pipe.InitBuffer(Q_x2, BUFFER_NUM, this->tileLength * sizeof(DTYPE_X2));
-        pipe.InitBuffer(Q_y, BUFFER_NUM, this->tileLength * sizeof(DTYPE_Y));
+        pipe.InitBuffer(Q_input_data, BUFFER_NUM, this->tileLength * sizeof(TYPE_INPUT_DATA));
+        pipe.InitBuffer(Q_x1, BUFFER_NUM, this->tileLength * sizeof(TYPE_X1));
+        pipe.InitBuffer(Q_x2, BUFFER_NUM, this->tileLength * sizeof(TYPE_X2));
+        pipe.InitBuffer(Q_y, BUFFER_NUM, this->tileLength * sizeof(TYPE_Y));
         pipe.InitBuffer(tmp1, this->tileLength * sizeof(half));
         pipe.InitBuffer(tmp2, this->tileLength * sizeof(half));
         this->value = Gm_value.GetValue(0);
@@ -35,22 +36,21 @@ public:
     __aicore__ inline void Process() {
         int32_t loopCount = this->tileNum;
         for (int32_t i = 0; i < loopCount-1; i++) {
-            this->length = this->tileLength;
-            CopyIn(i);
-            Compute(i);
-            CopyOut(i);
+            CopyIn(i, this->tileLength);
+            Compute(i, this->tileLength);
+            CopyOut(i, this->tileLength);
         }
-        this->length = this->blockLength - this->tileLength * (loopCount - 1);
-        CopyIn(loopCount - 1);
-        Compute(loopCount - 1);
-        CopyOut(loopCount - 1);
+        uint32_t length = this->blockLength - this->tileLength * (loopCount - 1);
+        CopyIn(loopCount - 1, length);
+        Compute(loopCount - 1, length);
+        CopyOut(loopCount - 1, length);
     }
 
 private:
-    __aicore__ inline void CopyIn(int32_t progress) {
-        LocalTensor<DTYPE_INPUT_DATA> input_data = Q_input_data.AllocTensor<DTYPE_INPUT_DATA>();
-        LocalTensor<DTYPE_X1> x1 = Q_x1.AllocTensor<DTYPE_X1>();
-        LocalTensor<DTYPE_X2> x2 = Q_x2.AllocTensor<DTYPE_X2>();
+    __aicore__ inline void CopyIn(int32_t progress, uint32_t length) {
+        LocalTensor<TYPE_INPUT_DATA> input_data = Q_input_data.AllocTensor<TYPE_INPUT_DATA>();
+        LocalTensor<TYPE_X1> x1 = Q_x1.AllocTensor<TYPE_X1>();
+        LocalTensor<TYPE_X2> x2 = Q_x2.AllocTensor<TYPE_X2>();
         DataCopy(input_data, Gm_input_data[progress * this->tileLength], length);
         DataCopy(x1, Gm_x1[progress * this->tileLength], length);
         DataCopy(x2, Gm_x2[progress * this->tileLength], length);
@@ -58,34 +58,34 @@ private:
         Q_x1.EnQue(x1);
         Q_x2.EnQue(x2);
     }
-    __aicore__ inline void Compute(int32_t progress) {
-        LocalTensor<DTYPE_INPUT_DATA> input_data = Q_input_data.DeQue<DTYPE_INPUT_DATA>();
-        LocalTensor<DTYPE_X1> x1 = Q_x1.DeQue<DTYPE_X1>();
-        LocalTensor<DTYPE_X2> x2 = Q_x2.DeQue<DTYPE_X2>();
-        LocalTensor<DTYPE_Y> y = Q_y.AllocTensor<DTYPE_Y>();
+    __aicore__ inline void Compute(int32_t progress, uint32_t length) {
+        LocalTensor<TYPE_INPUT_DATA> input_data = Q_input_data.DeQue<TYPE_INPUT_DATA>();
+        LocalTensor<TYPE_X1> x1 = Q_x1.DeQue<TYPE_X1>();
+        LocalTensor<TYPE_X2> x2 = Q_x2.DeQue<TYPE_X2>();
+        LocalTensor<TYPE_Y> y = Q_y.AllocTensor<TYPE_Y>();
         if constexpr (std::is_same_v<T, int8_t> || std::is_same_v<T, signed char>) {
             auto p1 = tmp1.Get<half>();
             auto p2 = tmp2.Get<half>();
             Cast(p1, x1, RoundMode::CAST_NONE, length);
             Cast(p2, x2, RoundMode::CAST_NONE, length);
             Mul(p1, p1, p2, length);
-            Muls(p1, p1, value, this->length);
+            Muls(p1, p1, value, length);
             Cast(p2, input_data, RoundMode::CAST_NONE, length);
             Add(p1, p1, p2, length);
             Cast(y, p1, RoundMode::CAST_NONE, length);
         }
         else {
             Mul(x1, x1, x2, length);
-            Muls(x1, x1, value, this->length);
+            Muls(x1, x1, value, length);
             Add(y, x1, input_data, length);
         }
         Q_input_data.FreeTensor(input_data);
         Q_x1.FreeTensor(x1);
         Q_x2.FreeTensor(x2);
-        Q_y.EnQue<DTYPE_Y>(y);
+        Q_y.EnQue<TYPE_Y>(y);
     }
-    __aicore__ inline void CopyOut(int32_t progress) {
-        LocalTensor<DTYPE_Y> y = Q_y.DeQue<DTYPE_Y>();
+    __aicore__ inline void CopyOut(int32_t progress, uint32_t length) {
+        LocalTensor<TYPE_Y> y = Q_y.DeQue<TYPE_Y>();
         DataCopy(Gm_y[progress * this->tileLength], y, length);
         Q_y.FreeTensor(y);
     }
@@ -95,20 +95,19 @@ private:
     TQue<QuePosition::VECIN, BUFFER_NUM> Q_input_data, Q_x1, Q_x2;
     TQue<QuePosition::VECOUT, BUFFER_NUM> Q_y;
     TBuf<QuePosition::VECCALC> tmp1, tmp2;
-    GlobalTensor<DTYPE_INPUT_DATA> Gm_input_data;
-    GlobalTensor<DTYPE_X1> Gm_x1;
-    GlobalTensor<DTYPE_X2> Gm_x2;
-    GlobalTensor<DTYPE_VALUE> Gm_value;
-    GlobalTensor<DTYPE_Y> Gm_y;
+    GlobalTensor<TYPE_INPUT_DATA> Gm_input_data;
+    GlobalTensor<TYPE_X1> Gm_x1;
+    GlobalTensor<TYPE_X2> Gm_x2;
+    GlobalTensor<TYPE_VALUE> Gm_value;
+    GlobalTensor<TYPE_Y> Gm_y;
     uint32_t blockLength;
     uint32_t tileNum;
     uint32_t tileLength;
-    uint32_t length;
-    Map<DTYPE_VALUE>::type value;
+    typename Map<TYPE_VALUE>::type value;
 };
 extern "C" __global__ __aicore__ void addcmul(GM_ADDR input_data, GM_ADDR x1, GM_ADDR x2, GM_ADDR value, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling) {
     GET_TILING_DATA(tiling_data, tiling);
-    KernelAddcmul<DTYPE_Y> op;
+    KernelAddcmul<DTYPE_INPUT_DATA, DTYPE_X1, DTYPE_X2, DTYPE_VALUE, DTYPE_Y> op;
     op.Init(input_data, x1, x2, value, y, tiling_data.totalLength, tiling_data.ALIGN_NUM, tiling_data.block_size, tiling_data.core_size, tiling_data.core_remain);
     op.Process();
 }

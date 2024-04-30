870e9e8bd1b05bdd67b0510b934e93c643696c55
smz <531483935@qq.com>
2024-04-30 14:28:28 +0800

    Make LPNormV2 shorter and faster

--- 08-LpNormV2/op_kernel/lp_norm_v2.cpp
+++ 08-LpNormV2/op_kernel/lp_norm_v2.cpp
@@ -28,46 +28,48 @@ public:
 
         // pipe alloc memory to queue, the unit is Bytes
         pipe.InitBuffer(inQueueX, BUFFER_NUM, this->tileLength * sizeof(T));
-        pipe.InitBuffer(outQueueZ, BUFFER_NUM, ALIGN_NUM * sizeof(T));
-        pipe.InitBuffer(tmpBuffer, this->tileLength * sizeof(float));
-        pipe.InitBuffer(tmpBuffer2, this->tileLength * sizeof(float));
-        pipe.InitBuffer(tmpBuffer3, this->tileLength * sizeof(float));
+        pipe.InitBuffer(SumQueue, 1, this->tileLength * sizeof(T));
+        pipe.InitBuffer(tmpBuffer, this->tileLength * sizeof(T));
+        pipe.InitBuffer(tmpBuffer2, this->tileLength * sizeof(T));
     }
     __aicore__ inline void Process()
     {
-        LocalTensor<T> zLocal = outQueueZ.AllocTensor<T>(); LocalTensor<T> zLocal2 = outQueueZ.AllocTensor<T>();
+        LocalTensor<T> sum = SumQueue.AllocTensor<T>();
         T zero = this->ptype == 3 ? 3.40282346638528859811704183484516925e+38F : 0;
-        Duplicate(zLocal, zero, this->ALIGN_NUM);
-        outQueueZ.EnQue<T>(zLocal); outQueueZ.EnQue<T>(zLocal2);
-        zLocal = outQueueZ.DeQue<T>(); zLocal2 = outQueueZ.DeQue<T>();
-        DataCopy(zGm, zLocal, this->ALIGN_NUM);
-        outQueueZ.FreeTensor(zLocal); outQueueZ.FreeTensor(zLocal2);
+        Duplicate(sum, zero, this->tileLength);
+        SumQueue.EnQue<T>(sum);
         // loop count need to be doubled, due to double buffer
         int32_t loopCount = this->tileNum;
         // tiling strategy, pipeline parallel
         for (int32_t i = 0; i < loopCount-1; i++) {
             CopyIn(i, this->tileLength);
-            ComputeFull(i, this->tileLength);
-            CopyOut(i);
+            Compute(i, this->tileLength);
         }
         auto length = this->blockLength - this->tileLength * (loopCount - 1);
         CopyIn(loopCount - 1, length);
         Compute(loopCount - 1, length - this->lastpadding);
-        CopyOut(loopCount - 1);
 
-        
-        zLocal = outQueueZ.AllocTensor<T>(); zLocal2 = outQueueZ.AllocTensor<T>();
-        DataCopy(zLocal, zGm, this->ALIGN_NUM);
-        if(this->ptype == 0){
-            T invp = this->invp;
-            Ln(zLocal, zLocal, 1);
-            Muls(zLocal, zLocal, invp, 1);
-            Exp(zLocal, zLocal, 1);
+        {
+            LocalTensor<T> sum = SumQueue.DeQue<T>();
+            LocalTensor<T> tmp = tmpBuffer.Get<T>();
+            if(this->ptype == 0){
+                ReduceSum(sum, sum, tmp, this->tileLength);
+                T invp = this->invp;
+                Ln(sum, sum, 1);
+                Muls(sum, sum, invp, 1);
+                Exp(sum, sum, 1);
+            }else if(this->ptype == 2){
+                ReduceMax(sum, sum, tmp, this->tileLength);
+            }else if(this->ptype == 3){
+                ReduceMin(sum, sum, tmp, this->tileLength);
+            }else{
+                ReduceSum(sum, sum, tmp, this->tileLength);
+            }
+            SumQueue.EnQue<T>(sum);
+            sum = SumQueue.DeQue<T>();
+            DataCopy(zGm, sum, this->ALIGN_NUM);
+            SumQueue.FreeTensor(sum);
         }
-        outQueueZ.EnQue<T>(zLocal); outQueueZ.EnQue<T>(zLocal2);
-        zLocal = outQueueZ.DeQue<T>(); zLocal2 = outQueueZ.DeQue<T>();
-        DataCopy(zGm, zLocal, this->ALIGN_NUM);
-        outQueueZ.FreeTensor(zLocal); outQueueZ.FreeTensor(zLocal2);
     }
 
 private:
@@ -85,176 +87,43 @@ private:
     {
         // deque input tensors from VECIN queue
         LocalTensor<T> xLocal = inQueueX.DeQue<T>();
-        LocalTensor<T> zLocal = outQueueZ.AllocTensor<T>();
-        LocalTensor<float> tmp = tmpBuffer.Get<float>();
-
-        if constexpr (std::is_same_v<T, half>) {
-            LocalTensor<float> xf = tmpBuffer2.Get<float>();
-            Cast(xf, xLocal, RoundMode::CAST_NONE, length);
-            Abs(xf, xf, length);
-            if(this->ptype == 0){
-                Ln(xf, xf, length);
-                Muls(xf, xf, this->p, length);
-                Exp(xf, xf, length);
-                ReduceSum(xf, xf, tmp, length);
-            }else if(this->ptype == 2){
-                ReduceMax(xf, xf, tmp, length);
-            }else if(this->ptype == 3){
-                ReduceMin(xf, xf, tmp, length);
-            }else if(this->ptype == 1){
-                LocalTensor<uint8_t> tmp2 = tmpBuffer3.Get<uint8_t>();
-                Duplicate(tmp, 0.0f, length);
-                Compare(tmp2, xf, tmp, CMPMODE::EQ, length);
-                Select(xf, tmp2, tmp, 1.0f, SELMODE::VSEL_TENSOR_SCALAR_MODE, length);
-                ReduceSum(xf, xf, tmp, length);
-                // Muls(xf, xf, -1.0f, length);
-                // LocalTensor<uint32_t> xuint = xf.template ReinterpretCast<uint32_t>();
-                // ShiftRight(xuint, xuint, 31u, length);
-                // LocalTensor<int32_t> xint = xuint.template ReinterpretCast<int32_t>();
-                // Cast(xf, xint, RoundMode::CAST_NONE, length);
-                // ReduceSum(xf, xf, tmp, length);
-            }
-            Cast(zLocal, xf, RoundMode::CAST_NONE, this->ALIGN_NUM);
-        }else{
-            Abs(xLocal, xLocal, length);
-            if(this->ptype == 0){
-                Ln(xLocal, xLocal, length);
-                Muls(xLocal, xLocal, this->p, length);
-                Exp(xLocal, xLocal, length);
-                ReduceSum(xLocal, xLocal, tmp, length);
-            }else if(this->ptype == 2){
-                ReduceMax(xLocal, xLocal, tmp, length);
-            }else if(this->ptype == 3){
-                ReduceMin(xLocal, xLocal, tmp, length);
-            }else if(this->ptype == 1){
-                LocalTensor<uint8_t> tmp2 = tmpBuffer3.Get<uint8_t>();
-                Duplicate(tmp, 0.0f, length);
-                Compare(tmp2, xLocal, tmp, CMPMODE::EQ, length);
-                Select(xLocal, tmp2, tmp, 1.0f, SELMODE::VSEL_TENSOR_SCALAR_MODE, length);
-                ReduceSum(xLocal, xLocal, tmp, length);
-                // Muls(xLocal, xLocal, -1.0f, length);
-                // LocalTensor<uint32_t> xuint = xLocal.template ReinterpretCast<uint32_t>();
-                // ShiftRight(xuint, xuint, 31u, length);
-                // LocalTensor<int32_t> xint = xuint.template ReinterpretCast<int32_t>();
-                // Cast(xLocal, xint, RoundMode::CAST_NONE, length);
-                // ReduceSum(xLocal, xLocal, tmp, length);
-            }
-            DataCopy(zLocal, xLocal, this->ALIGN_NUM);
-        }
+        LocalTensor<T> sum = SumQueue.DeQue<T>();
         
-
-        // enque the output tensor to VECOUT queue
-        outQueueZ.EnQue<T>(zLocal);
-        // free input tensors for reuse
-        inQueueX.FreeTensor(xLocal);
-    }
-    __aicore__ inline void ComputeFull(int32_t progress, uint32_t length)
-    {
-        // deque input tensors from VECIN queue
-        LocalTensor<T> xLocal = inQueueX.DeQue<T>();
-        LocalTensor<T> zLocal = outQueueZ.AllocTensor<T>();
-        LocalTensor<float> tmp = tmpBuffer.Get<float>();
-
-        if constexpr (std::is_same_v<T, half>) {
-            LocalTensor<float> xf = tmpBuffer2.Get<float>();
-            Cast(xf, xLocal, RoundMode::CAST_NONE, length);
-            Abs(xf, xf, length);
-            if(this->ptype == 0){
-                Ln(xf, xf, length);
-                Muls(xf, xf, this->p, length);
-                Exp(xf, xf, length);
-                uint64_t mask = 32 / 4 * 8;
-                uint64_t repeat = (length + mask - 1) / mask;
-                WholeReduceSum<float>(xf, xf, mask, repeat, 1, 1, 8);
-                WholeReduceSum<float>(xf, xf, repeat, 1, 1, 1, 8);
-            }else if(this->ptype == 2){
-                ReduceMax(xf, xf, tmp, length);
-            }else if(this->ptype == 3){
-                ReduceMin(xf, xf, tmp, length);
-            }else if(this->ptype == 1){
-                LocalTensor<uint8_t> tmp2 = tmpBuffer3.Get<uint8_t>();
-                Duplicate(tmp, 0.0f, length);
-                Compare(tmp2, xf, tmp, CMPMODE::EQ, length);
-                Select(xf, tmp2, tmp, 1.0f, SELMODE::VSEL_TENSOR_SCALAR_MODE, length);
-                ReduceSum(xf, xf, tmp, length);
-                // Muls(xf, xf, -1.0f, length);
-                // LocalTensor<uint32_t> xuint = xf.template ReinterpretCast<uint32_t>();
-                // ShiftRight(xuint, xuint, 31u, length);
-                // LocalTensor<int32_t> xint = xuint.template ReinterpretCast<int32_t>();
-                // Cast(xf, xint, RoundMode::CAST_NONE, length);
-                // ReduceSum(xf, xf, tmp, length);
-            }
-            Cast(zLocal, xf, RoundMode::CAST_NONE, this->ALIGN_NUM);
-        }else{
-            Abs(xLocal, xLocal, length);
-            if(this->ptype == 0){
-                Ln(xLocal, xLocal, length);
-                Muls(xLocal, xLocal, this->p, length);
-                Exp(xLocal, xLocal, length);
-                uint64_t mask = this->ALIGN_NUM * 8;
-                uint64_t repeat = (length + mask - 1) / mask;
-                WholeReduceSum<T>(xLocal, xLocal, mask, repeat, 1, 1, 8);
-                WholeReduceSum<T>(xLocal, xLocal, repeat, 1, 1, 1, 8);
-            }else if(this->ptype == 2){
-                ReduceMax(xLocal, xLocal, tmp, length);
-            }else if(this->ptype == 3){
-                ReduceMin(xLocal, xLocal, tmp, length);
-            }else if(this->ptype == 1){
-                LocalTensor<uint8_t> tmp2 = tmpBuffer3.Get<uint8_t>();
-                Duplicate(tmp, 0.0f, length);
-                Compare(tmp2, xLocal, tmp, CMPMODE::EQ, length);
-                Select(xLocal, tmp2, tmp, 1.0f, SELMODE::VSEL_TENSOR_SCALAR_MODE, length);
-                ReduceSum(xLocal, xLocal, tmp, length);
-                // Muls(xLocal, xLocal, -1.0f, length);
-                // LocalTensor<uint32_t> xuint = xLocal.template ReinterpretCast<uint32_t>();
-                // ShiftRight(xuint, xuint, 31u, length);
-                // LocalTensor<int32_t> xint = xuint.template ReinterpretCast<int32_t>();
-                // Cast(xLocal, xint, RoundMode::CAST_NONE, length);
-                // ReduceSum(xLocal, xLocal, tmp, length);
-            }
-            DataCopy(zLocal, xLocal, this->ALIGN_NUM);
-        }
-        
-
-        // enque the output tensor to VECOUT queue
-        outQueueZ.EnQue<T>(zLocal);
-        // free input tensors for reuse
-        inQueueX.FreeTensor(xLocal);
-    }
-    __aicore__ inline void CopyOut(int32_t progress)
-    {
-        // deque output tensor from VECOUT queue
-        LocalTensor<T> zLocal = outQueueZ.DeQue<T>();
-        // copy progress_th tile from local tensor to global tensor
-        if(this->ptype == 2){
-            SetAtomicMax<T>();
-            DataCopy(zGm, zLocal, this->ALIGN_NUM);
-            SetAtomicNone();
+        Abs(xLocal, xLocal, length);
+        if(this->ptype == 0){
+            Ln(xLocal, xLocal, length);
+            Muls(xLocal, xLocal, this->p, length);
+            Exp(xLocal, xLocal, length);
+            Add(sum, xLocal, sum, length);
+        }else if(this->ptype == 2){
+            Max(sum, xLocal, sum, length);
         }else if(this->ptype == 3){
-            SetAtomicMin<T>();
-            DataCopy(zGm, zLocal, this->ALIGN_NUM);
-            SetAtomicNone();
-        }else{
-            SetAtomicAdd<T>();
-            DataCopy(zGm, zLocal, this->ALIGN_NUM);
-            SetAtomicNone();
+            Min(sum, xLocal, sum, length);
+        }else if(this->ptype == 1){
+            LocalTensor<T> tmp = tmpBuffer.Get<T>();
+            LocalTensor<uint8_t> tmp2 = tmpBuffer2.Get<uint8_t>();
+            Duplicate(tmp, (T)0.0, length);
+            Compare(tmp2, xLocal, tmp, CMPMODE::EQ, length);
+            Select(xLocal, tmp2, tmp, (T)1.0, SELMODE::VSEL_TENSOR_SCALAR_MODE, length);
+            Add(sum, xLocal, sum, length);
         }
         
-        // free output tensor for reuse
-        outQueueZ.FreeTensor(zLocal);
+        SumQueue.EnQue<T>(sum);
+        // free input tensors for reuse
+        inQueueX.FreeTensor(xLocal);
     }
 
 private:
     TPipe pipe;
-    TBuf<QuePosition::VECCALC> tmpBuffer, tmpBuffer2, tmpBuffer3;
+    TBuf<QuePosition::VECCALC> tmpBuffer, tmpBuffer2;
     // create queues for input, in this case depth is equal to buffer num
     TQue<QuePosition::VECIN, BUFFER_NUM> inQueueX;
     // create queue for output, in this case depth is equal to buffer num
-    TQue<QuePosition::VECOUT, BUFFER_NUM> outQueueZ;
+    TQue<QuePosition::VECOUT, 1> SumQueue;
     GlobalTensor<T> xGm;
     GlobalTensor<T> zGm;
-    float p;
-    float invp;
+    T p;
+    T invp;
     uint32_t ptype;
     uint32_t ALIGN_NUM;
     uint32_t blockLength;
@@ -348,7 +217,7 @@ private:
 
 private:
     TPipe pipe;
-    TBuf<QuePosition::VECCALC> castBuffer, tmpBuffer;
+    TBuf<QuePosition::VECCALC> tmpBuffer;
     // create queues for input, in this case depth is equal to buffer num
     TQue<QuePosition::VECIN, BUFFER_NUM> inQueueX;
     TQue<QuePosition::VECOUT, 1> SumQueue;
